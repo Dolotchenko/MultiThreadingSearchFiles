57729aa212bad83a5bc56bec7bb3caa2c42f15a8
Dolotchenko Maxim <maximdolotchenko@yandex.ru>
2019-08-26 02:20:47 +0500

    Многопоточный поиск файлов в директориях ч7

--- Tests/FileWriterTest.java
+++ Tests/FileWriterTest.java
@@ -1,16 +1,28 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import java.util.concurrent.ConcurrentSkipListSet;
+
 
 class FileWriterTest {
-    FileWriter fileWriter;
+    private FileWriter fileWriter;
+    ConcurrentSkipListSet<String> allFiles;
+
 
     @BeforeEach
     public void init() {
         fileWriter = new FileWriter();
+        allFiles = new ConcurrentSkipListSet<>();
 
     }
 
+
+
     @Test
     void fileWriter() {
+        allFiles.add("Тестовое слово");
+        Assertions.assertTrue(allFiles.size()>0);
+        fileWriter.fileWriter(allFiles);
+
     }
 }
